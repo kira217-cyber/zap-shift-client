b24c52380316c87eadc7deb2e71b12283829c53e
kira217-cyber <gamingkira217@gmail.com>
2025-06-20 00:54:05 +0600

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "../pages/Home/Home/Home";
+import { Outlet } from "react-router";
+import Navbar from "../shared/Navbar/Navbar";
+import Footer from "../shared/Footer/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    
+    </div>
+  );
+};
+
+export default MainLayout;
